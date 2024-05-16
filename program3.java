3288983b4bcfca3a88f307c82a5170a9a5ba1257
Rajeev Ranjan <159820641+RajeevRanjansing@users.noreply.github.com>
2024-05-16 23:14:20 +0530

    Add files via upload

---
+++ program3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class program3{
+    public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter meter");
+    double meter=sc.nextDouble();
+
+    double feet=meter*3.28084;
+
+    System.out.println("feet is "+feet);
+
+}
+}
